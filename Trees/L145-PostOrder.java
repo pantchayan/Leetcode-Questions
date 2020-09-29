aeae3f8686dbb90b104232433b15f9749dbd9cb1
pantchayan <pantchayan@gmail.com>
2020-09-29 21:25:42 +0530

    29th Sept

---
+++ Trees/L145-PostOrder.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        preorder(root, ans);
+
+        return ans;
+    }
+
+    public static void preorder(TreeNode node, List<Integer> ans) {
+        if (node == null)
+            return;
+
+        ans.add(node.val);
+        preorder(node.left, ans);
+        preorder(node.right, ans);
+    }
+
+}
